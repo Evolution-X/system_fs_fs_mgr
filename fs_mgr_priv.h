master-hax/aosp-system-core

Add support for max_comp_streams option for zram
With this option the fstab can include a parameter for zram
the enables more streams for kernels that does not have
dynamic stream task handling.

Test: Made sure max_comp_streams is set according to fstab.
Change-Id: I22e158a075a8a86b4e80d3b13989896929c0d223

fs_mgr/fs_mgr_priv.h
@@ -85,6 +85,7 @@ __BEGIN_DECLS
 #define MF_FORCEFDEORFBE 0x10000
 #define MF_LATEMOUNT    0x20000
 #define MF_NOFAIL       0x40000
+#define MF_MAX_COMP_STREAMS 0x80000
 
 #define DM_BUF_SIZE 4096
 

Evolution-X/system_fs_fs_mgr

fs_mgr_priv.h
@@ -86,6 +86,7 @@ __BEGIN_DECLS
 #define MF_LATEMOUNT    0x20000
 #define MF_NOFAIL       0x40000
 #define MF_VERIFYATBOOT 0x80000
+#define MF_MAX_COMP_STREAMS 0x100000
 
 #define DM_BUF_SIZE 4096
 